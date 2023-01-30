a7a66281e3da19e40c1a0426c797ecac416e8380
Md. Shahadat Sarker <me.sarker@gmail.com>
2023-01-30 09:22:06 +0600

    bucket api - done

--- src/index.ts
+++ src/index.ts
@@ -35,8 +35,19 @@ interface IUpload {
   folder?: string;
 }
 
+interface IBucket {
+  apiKey: string;
+  apiSecretKey: string;
+  region: string;
+  title: string;
+  isPrivate?: boolean;
+}
+
 interface IAmarCDN {
   upload: ({ apiKey, apiSecretKey, region, bucket, isPrivate, folder }: IUpload) => void;
+  createBucket: ({ apiKey, apiSecretKey, region, title, isPrivate }: IBucket) => void;
+  getBucketList: ({ apiKey, apiSecretKey, region, title, isPrivate }: any) => void;
+  deleteBucket: ({ apiKey, apiSecretKey, region, bucketTitle, bucketId }: any) => void;
 }
 export class AmarCDN implements IAmarCDN {
   async upload({ apiKey, apiSecretKey, region, bucket, files, isPrivate, folder }: IUpload): Promise<any> {
@@ -64,6 +75,124 @@ export class AmarCDN implements IAmarCDN {
       };
 
       url = 'https://' + url.regionUrl + '/file/upload-many';
+
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          ...headers,
+        },
+        body: formData,
+      }).catch((err) => {
+        throw new Error(err.msg);
+      });
+      const response = await res.json();
+
+      if (response.code === 200) return response;
+
+      throw new Error(response.msg);
+    } catch (err) {
+      throw new Error('Something went wrong');
+    }
+  }
+
+  async createBucket({ apiKey, apiSecretKey, region, title, isPrivate }: IBucket): Promise<any> {
+    if (!apiKey) throw new Error('Api Key Id is required!');
+    if (!apiSecretKey) throw new Error('Api Secret Key is required!');
+    if (!region) throw new Error('Region is required!');
+    if (!title) throw new Error('Bucket title is required!');
+
+    try {
+      const formData = new FormData();
+      formData.append('bucket', title);
+      formData.append('region', region);
+      formData.append('isPrivate', isPrivate ? 'YES' : 'NO');
+
+      let url: any = CDNINFO.find((re) => re.regionKey === region) || null;
+      if (!url) throw new Error('Region key is not valid!');
+
+      const headers: any = {
+        'api-key': apiKey,
+        'api-secret-key': apiSecretKey,
+        rid: url.regionId,
+      };
+
+      url = 'https://' + url.regionUrl + '/bucket/create';
+
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          ...headers,
+        },
+        body: formData,
+      }).catch((err) => {
+        throw new Error(err.msg);
+      });
+      const response = await res.json();
+
+      if (response.code === 200) return response;
+
+      throw new Error(response.msg);
+    } catch (err) {
+      throw new Error('Something went wrong');
+    }
+  }
+
+  async getBucketList({ apiKey, apiSecretKey, region }: any): Promise<any> {
+    if (!apiKey) throw new Error('Api Key Id is required!');
+    if (!apiSecretKey) throw new Error('Api Secret Key is required!');
+    if (!region) throw new Error('Region is required!');
+
+    try {
+      let url: any = CDNINFO.find((re) => re.regionKey === region) || null;
+      if (!url) throw new Error('Region key is not valid!');
+
+      const headers: any = {
+        'api-key': apiKey,
+        'api-secret-key': apiSecretKey,
+        rid: url.regionId,
+      };
+
+      url = 'https://' + url.regionUrl + '/bucket/list';
+
+      const res = await fetch(url, {
+        method: 'GET',
+        headers: {
+          ...headers,
+        },
+      }).catch((err) => {
+        throw new Error(err.msg);
+      });
+      const response = await res.json();
+
+      if (response.code === 200) return response;
+
+      throw new Error(response.msg);
+    } catch (err) {
+      throw new Error('Something went wrong');
+    }
+  }
+
+  async deleteBucket({ apiKey, apiSecretKey, region, bucketTitle, bucketId }: any): Promise<any> {
+    if (!apiKey) throw new Error('Api Key Id is required!');
+    if (!apiSecretKey) throw new Error('Api Secret Key is required!');
+    if (!region) throw new Error('Region is required!');
+
+    try {
+      const formData = new FormData();
+      formData.append('bucket', bucketTitle);
+      formData.append('bucketId', bucketId);
+      formData.append('region', region);
+
+      let url: any = CDNINFO.find((re) => re.regionKey === region) || null;
+      if (!url) throw new Error('Region key is not valid!');
+
+      const headers: any = {
+        'api-key': apiKey,
+        'api-secret-key': apiSecretKey,
+        rid: url.regionId,
+      };
+
+      url = 'https://' + url.regionUrl + '/bucket/delete';
 
       const res = await fetch(url, {
         method: 'POST',
